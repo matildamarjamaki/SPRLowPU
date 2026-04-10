cee69110e6702d383cfca5e662ef4551f28a173d
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2026-04-10 10:41:31 +0200

    Choose PFBlocks with one track and at least one HCAL cluster. Make first track plots.

--- RECOAnalysis/plugins/RECOAnalyzer.cc
+++ RECOAnalysis/plugins/RECOAnalyzer.cc
@@ -1,5 +1,6 @@
 // system include files
 #include <memory>
+#include <utility>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -10,6 +11,9 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 
@@ -25,6 +29,13 @@
 #include "DataFormats/ParticleFlowReco/interface/PFClusterFwd.h"
 #include "DataFormats/ParticleFlowReco/interface/PFCluster.h"
 
+#include "TH1.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TH3F.h"
+#include "TProfile.h"
+#include "TProfile2D.h"
+
 
 class RECOAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources> {
 public:
@@ -38,14 +49,62 @@ private:
   void analyze(const edm::Event&, const edm::EventSetup&) override;
   void endJob() override;
 
+  edm::Service<TFileService> fs_;
+
+  std::map<std::string, TH1F*>       m_mapH1D;
+  std::map<std::string, TH2F*>       m_mapH2D;
+  std::map<std::string, TH3F*>       m_mapH3D;
+  std::map<std::string, TProfile*>   m_mapProf;
+  std::map<std::string, TProfile2D*> m_mapProf2D;
+
+  template <typename T, typename... Args>
+  T* book(Args&&... args) const {
+    T* t = fs_->make<T>(std::forward<Args>(args)...);
+    return t;
+  }
+
+  template <typename... Args>
+  void bookTH1F(const std::string& hName, const std::string& hTitle, Args&&... args){
+    m_mapH1D[hName] = book<TH1F>(hName.c_str(),hTitle.c_str(),std::forward<Args>(args)...);
+  }
+  template <typename... Args>
+  void bookTH2F(const std::string& hName, const std::string& hTitle, Args&&... args){
+    m_mapH2D[hName] = book<TH2F>(hName.c_str(),hTitle.c_str(),std::forward<Args>(args)...);
+  }
+  template <typename... Args>
+  void bookTH3F(const std::string& hName, const std::string& hTitle, Args&&... args){
+    m_mapH3D[hName] = book<TH3F>(hName.c_str(),hTitle.c_str(),std::forward<Args>(args)...);
+  }
+  template <typename... Args>
+  void bookProfile(const std::string& pName,const std::string& pTitle, Args&&... args){
+    m_mapProf[pName] = book<TProfile>(pName.c_str(),pTitle.c_str(),std::forward<Args>(args)...);
+  }
+  template <typename... Args>
+  void bookProfile2D(const std::string& pName,const std::string& pTitle, Args&&... args){
+    m_mapProf2D[pName] = book<TProfile2D>(pName.c_str(),pTitle.c_str(),std::forward<Args>(args)...);
+  }
+
   // ----------member data ---------------------------
-   edm::EDGetTokenT<std::vector<reco::PFBlock>> pfBlocksToken_;
+  edm::EDGetTokenT<std::vector<reco::PFBlock>> pfBlocksToken_;
+  edm::EDGetTokenT<reco::VertexCollection> primaryVerticesToken_;
 };
 
 
+
 RECOAnalyzer::RECOAnalyzer(const edm::ParameterSet& iConfig):
-  pfBlocksToken_(consumes<std::vector<reco::PFBlock>>(iConfig.getParameter<edm::InputTag>("pfBlock"))){
+  pfBlocksToken_(consumes<std::vector<reco::PFBlock>>(iConfig.getParameter<edm::InputTag>("pfBlock"))),
+  primaryVerticesToken_(consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("primaryVertices"))){
   //now do what ever initialization is needed
+
+  usesResource(TFileService::kSharedResource);
+
+  bookTH1F("h_nPV",";nPV",         80, 0, 80);
+  bookTH1F("h_nPVGood",";nPVGood", 80, 0, 80);
+
+  bookTH1F("h_pftrack_pt",";PF track pT [GeV]", 50, 0., 50.);
+  bookTH1F("h_pftrack_eta",";PF track #eta",   100, -5., 5.);
+
+  bookTH2F("h2_pftrack_eta_vs_pt",";PF track #eta;PF track pT [GeV]", 100, -5., 5., 50, 0., 50.);
 }
 
 RECOAnalyzer::~RECOAnalyzer() {
@@ -58,6 +117,21 @@ RECOAnalyzer::~RECOAnalyzer() {
 // ------------ method called for each event  ------------
 void RECOAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
+  edm::Handle<reco::VertexCollection> primaryVerticesHandle;
+  iEvent.getByToken(primaryVerticesToken_, primaryVerticesHandle);
+  int nPV = 0;
+  int nPVGood = 0;
+  for (unsigned int ind = 0; ind < primaryVerticesHandle->size(); ind++) {
+    const reco::Vertex& pv =  (*primaryVerticesHandle)[ind];
+    nPV++;
+    bool pass = (!pv.isFake()) && pv.ndof() > 4 && abs(pv.z()) <= 24 && pv.position().Rho() <= 2;
+    if (pass) {
+      nPVGood++;
+    }
+  }
+  m_mapH1D["h_nPV"]->Fill(nPV);
+  m_mapH1D["h_nPVGood"]->Fill(nPVGood);
+
   //
   // Get PFBlocks
   //
@@ -68,7 +142,7 @@ void RECOAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
   //
   // Loop over PFBlocks
   //
-  std::list<reco::PFBlockRef> singleTrackBlockRefs;
+  std::list<reco::PFBlockRef> singleTrackWithHCALBlockRefs;
 
   for (unsigned iBlock = 0; iBlock < pfBlocks.size(); ++iBlock) {
     reco::PFBlockRef blockref = reco::PFBlockRef(pfBlocksHandle, iBlock);
@@ -113,10 +187,41 @@ void RECOAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
         nHO++;
       }
     }
-    if(nTrack == 1){
-      singleTrackBlockRefs.push_back(blockref);
+    if(nTrack == 1 && nHCAL >= 1){
+      singleTrackWithHCALBlockRefs.push_back(blockref);
     }
   }
+
+  //
+  // Loop over the singleTrackWithHCALBlockRefs
+  //
+  for (const reco::PFBlockRef& blockRef : singleTrackWithHCALBlockRefs) {
+    if (blockRef.isNull())
+      continue;
+    //
+    // Get the elements and loop over them
+    //
+    const reco::PFBlock& block = *blockRef;
+    const auto& elements = block.elements();
+
+    for (unsigned i = 0; i < elements.size(); ++i) {
+      //
+      // Find the tracks. Should be only one track
+      //
+      if (elements[i].type() == reco::PFBlockElement::TRACK) {
+        reco::TrackRef trk = elements[i].trackRef();
+        if (trk.isNonnull()) {
+          float trk_pt = trk->pt();
+          float trk_eta = trk->eta();
+          if (trk_pt < 3.) continue;
+          m_mapH1D["h_pftrack_pt"]->Fill(trk_pt);
+          m_mapH1D["h_pftrack_eta"]->Fill(trk_eta);
+          m_mapH2D["h2_pftrack_eta_vs_pt"]->Fill(trk_eta,trk_pt);
+        }
+      }
+    }
+  }
+
 }
 
 // ------------ method called once each job just before starting event loop  ------------
--- RECOAnalysis/python/ConfFile_cfg.py
+++ RECOAnalysis/python/ConfFile_cfg.py
@@ -5,19 +5,32 @@ process = cms.Process("PFANA")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet(
-   input = cms.untracked.int32(1)
+   input = cms.untracked.int32(-1)
 )
 
+#
+#
+#
 process.source = cms.Source("PoolSource",
    fileNames = cms.untracked.vstring(
       'file:root://hip-cms-se.csc.fi//store/user/nbinnorj/RAWToRECO_2025_v0p3/CRABOUTPUT/SpecialZeroBias1/Run2025G_PromptRECO_v1_LowPUJSON_RAWToRECO_2025_v0p3/260320_213619/0000/RECO_1.root'
    )
 )
 
+
+#
+#
+#
+process.TFileService = cms.Service("TFileService",
+   fileName = cms.string(f'histo_SPRLowPU.root')
+)
+
+
 process.recoAnalyzer = cms.EDAnalyzer(
-   # 'RECOAnalyzer',
-   'RECODumper',
-   pfBlock   = cms.InputTag('particleFlowBlock')
+   'RECOAnalyzer',
+   # 'RECODumper',
+   pfBlock   = cms.InputTag('particleFlowBlock'),
+   primaryVertices = cms.InputTag('offlinePrimaryVertices'),
 )
 
 process.p = cms.Path(process.recoAnalyzer)
